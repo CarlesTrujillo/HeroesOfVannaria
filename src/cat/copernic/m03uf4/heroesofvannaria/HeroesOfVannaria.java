5d541a11fb1af3019f8a653d557aa043b3fa0c2b
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-05-27 21:14:30 +0200

    Cambios menores a la aplicacion

--- src/cat/copernic/m03uf4/heroesofvannaria/HeroesOfVannaria.java
+++ src/cat/copernic/m03uf4/heroesofvannaria/HeroesOfVannaria.java
@@ -38,7 +38,7 @@ public class HeroesOfVannaria {
         joc.juego();
     }
 
-    public static void juego() {
+    public void juego() {
         Scanner in = new Scanner(System.in);
 
         boolean finalJoc = false;
@@ -63,6 +63,7 @@ public class HeroesOfVannaria {
                 case "X":
                 case "x":
                     finalJoc = true;
+                    guardarPersonajes();
                     break;
                 default:
                     System.out.println("Opcio incorrecta!");
@@ -70,27 +71,66 @@ public class HeroesOfVannaria {
         }
     }
 
-    public static void crearPersonaje() {
+    public void crearPersonaje() {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Escull el nom del teu personatge: ");
         String nom = in.nextLine();
 
         boolean claseValida = false;
+        boolean armaValida = false;
 
-        System.out.print("Selecciona la clase: ");
-        String clase = in.nextLine();
+        while (!claseValida) {
+            System.out.println("1. Guerrer");
+            System.out.println("2. Cavaller");
+            System.out.println("3. Valquiria");
+            System.out.println("4. Assassi");
 
-        System.out.print("Selecciona la teva arma: ");
-        String arma = in.nextLine();
+            System.out.print("Selecciona la clase: ");
+            String clase = in.nextLine();
+
+            switch (clase) {
+                case "Guerrer":
+                    break;
+                case "Cavaller":
+                    break;
+                case "Valquiria":
+                    break;
+                case "Assassi":
+                    break;
+                default:
+                    System.out.println("Clase no valida!");
+                    claseValida = true;
+            }
+        }
+        while (!armaValida) {
+            System.out.println("1. Daga");
+            System.out.println("2. Espasa");
+            System.out.println("3. Martell de combat");
+
+            System.out.print("Selecciona la teva arma: ");
+            String arma = in.nextLine();
+
+            switch (arma) {
+                case "Daga":
+                    break;
+                case "Espasa":
+                    break;
+                case "Martell de combat":
+                    break;
+                default:
+                    System.out.println("Arma no valida!");
+            }
+        }
+        System.out.println("Posa en un maxim de 60 punts, els atributs del teu personatge...");
+        int forca, cons, vel, inte, sor;
+    }
+
+    public void combat() {
 
     }
 
-    public static void combat() {
-
-    }
-
-    public static void tirada() {
+    public void tirada() {
         Dado dau1 = new Dado(25);
         Dado dau2 = new Dado(25);
         Dado dau3 = new Dado(25);
@@ -161,6 +201,10 @@ public class HeroesOfVannaria {
         }
 
         return personatges;
+
+    }
+
+    public void guardarPersonajes() {
 
     }
 }
